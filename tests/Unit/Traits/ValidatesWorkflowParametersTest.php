4cf687e66ba8b9c19b70a1f64ce149752ff4f064
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-04-22 19:24:06 -0400

    25: Write Tests For Validating Workflow Parameters

---
+++ tests/Unit/Traits/ValidatesWorkflowParametersTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Workflowable\Workflow\Tests\Unit\Traits;
+
+use Workflowable\Workflow\Tests\TestCase;
+use Workflowable\Workflow\Traits\ValidatesWorkflowParameters;
+
+class ValidatesWorkflowParametersTest extends TestCase
+{
+    public function test_it_can_validate_workflow_parameters(): void
+    {
+        $class = new class {
+            use ValidatesWorkflowParameters;
+
+            public function __construct()
+            {
+                $this->parameters = [
+                    'test' => 'test'
+                ];
+            }
+
+            public function getRules(): array
+            {
+                return [
+                    'test' => 'required|string|min:3'
+                ];
+            }
+        };
+
+        $this->assertTrue($class->hasValidParameters());
+
+        $invalid = new class {
+            use ValidatesWorkflowParameters;
+
+            public function __construct()
+            {
+                $this->parameters = [
+                    'test' => 'test'
+                ];
+            }
+
+            public function getRules(): array
+            {
+                return [
+                    'test' => 'required|int'
+                ];
+            }
+        };
+
+        $this->assertFalse($invalid->hasValidParameters());
+    }
+
+    public function test_it_can_get_the_workflow_parameters(): void
+    {
+        $class = new class {
+            use ValidatesWorkflowParameters;
+
+            public function __construct()
+            {
+                $this->parameters = [
+                    'test' => 'test'
+                ];
+            }
+
+            public function getRules(): array
+            {
+                return [
+                    'test' => 'required|int'
+                ];
+            }
+        };
+
+        $this->assertEquals(['test' => 'test'], $class->getParameters());
+    }
+}
