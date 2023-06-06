14407f895b76a08cd13852b4a27bf903e0f8e668
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-06-06 12:36:42 -0400

    Update WorkflowTransitionData.php

--- src/DataTransferObjects/WorkflowTransitionData.php
+++ src/DataTransferObjects/WorkflowTransitionData.php
@@ -39,7 +39,7 @@ class WorkflowTransitionData
         $workflowTransitionData->uxUuid = $data['ux_uuid'];
         $workflowTransitionData->fromWorkflowStep = $data['from_workflow_step'];
         $workflowTransitionData->toWorkflowStep = $data['to_workflow_step'];
-        $workflowTransitionData->workflowConditions = collect($data['workflow_conditions'])
+        $workflowTransitionData->workflowConditions = collect($data['workflow_conditions'] ?? [])
             ->map(function ($workflowCondition) {
                 return WorkflowConditionData::fromArray($workflowCondition);
             });
