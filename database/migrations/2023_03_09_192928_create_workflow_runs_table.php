be4b97a9b3f88e115b003e2a8e5f861d822db0f7
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-06-06 08:16:16 -0400

    Check-in

--- database/migrations/2023_03_09_192928_create_workflow_runs_table.php
+++ database/migrations/2023_03_09_192928_create_workflow_runs_table.php
@@ -26,7 +26,6 @@ return new class extends Migration
                 ->nullable()
                 ->constrained('workflow_steps')
                 ->cascadeOnDelete();
-            $table->json('parameters');
             $table->timestamp('first_run_at')->nullable();
             $table->timestamp('last_run_at')->nullable();
             $table->timestamp('next_run_at')->useCurrent();
