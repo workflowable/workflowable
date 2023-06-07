77457c819121f9e33ae6c094e0a2e5cae78e66ec
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-06-07 13:28:02 -0400

    Testing Scaffolding

---
+++ tests/Unit/Actions/WorkflowConditions/UpdateWorkflowConditionActionTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Workflowable\Workflow\Tests\Unit\Actions\WorkflowConditions;
+
+use Workflowable\Workflow\Tests\TestCase;
+
+class UpdateWorkflowConditionActionTest extends TestCase
+{
+
+}
