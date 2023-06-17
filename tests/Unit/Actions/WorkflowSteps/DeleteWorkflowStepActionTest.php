2fa7c146873bf265ad60ebc10a66441ea50dd0e4
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-06-17 19:12:40 -0400

    Clean up and Identifying Needed Test Cases

---
+++ tests/Unit/Actions/WorkflowSteps/DeleteWorkflowStepActionTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Workflowable\Workflow\Tests\Unit\Actions\WorkflowSteps;
+
+use Workflowable\Workflow\Tests\TestCase;
+
+class DeleteWorkflowStepActionTest extends TestCase
+{
+    public function test_that_we_can_delete_a_workflow_step(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/Unit/Actions/WorkflowTransitions/DeleteWorkflowTransitionActionTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Workflowable\Workflow\Tests\Unit\Actions\WorkflowTransitions;
+
+use Workflowable\Workflow\Tests\TestCase;
+
+class DeleteWorkflowTransitionActionTest extends TestCase
+{
+    public function test_that_we_can_delete_a_workflow_transition(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
