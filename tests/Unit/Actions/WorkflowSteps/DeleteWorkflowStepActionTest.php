1d1d5048d41d2f93526b4b2a65401e1becdf9d91
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-06-20 07:57:41 -0400

    Deletion Test

--- tests/Unit/Actions/WorkflowSteps/DeleteWorkflowStepActionTest.php
+++ tests/Unit/Actions/WorkflowSteps/DeleteWorkflowStepActionTest.php
@@ -2,12 +2,37 @@
 
 namespace Workflowable\Workflow\Tests\Unit\Actions\WorkflowSteps;
 
+use Workflowable\Workflow\Actions\WorkflowSteps\DeleteWorkflowStepAction;
+use Workflowable\Workflow\Exceptions\WorkflowException;
+use Workflowable\Workflow\Models\WorkflowStatus;
+use Workflowable\Workflow\Models\WorkflowStep;
 use Workflowable\Workflow\Tests\TestCase;
+use Workflowable\Workflow\Tests\Traits\HasWorkflowRunTests;
 
 class DeleteWorkflowStepActionTest extends TestCase
 {
+    use HasWorkflowRunTests;
+
     public function test_that_we_can_delete_a_workflow_step(): void
     {
-        $this->markTestIncomplete();
+        $this->workflow->update(['workflow_status_id' => WorkflowStatus::DRAFT]);
+
+        /** @var DeleteWorkflowStepAction $deleteWorkflowStepAction */
+        $deleteWorkflowStepAction = app(DeleteWorkflowStepAction::class);
+        $deleteWorkflowStepAction->handle($this->fromWorkflowStep);
+
+        $this->assertDatabaseMissing(WorkflowStep::class, [
+            'id' => $this->fromWorkflowStep->id,
+        ]);
+    }
+
+    public function test_that_we_cannot_delete_a_workflow_step_from_an_active_workflow(): void
+    {
+        $this->expectException(WorkflowException::class);
+        $this->expectExceptionMessage(WorkflowException::cannotModifyWorkflowNotInDraftState()->getMessage());
+
+        /** @var DeleteWorkflowStepAction $deleteWorkflowStepAction */
+        $deleteWorkflowStepAction = app(DeleteWorkflowStepAction::class);
+        $deleteWorkflowStepAction->handle($this->fromWorkflowStep);
     }
 }
