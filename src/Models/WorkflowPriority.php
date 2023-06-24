26460f54bc688eb9ece7bbef0ecb58097af102ba
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-06-24 11:14:20 -0400

    Amendments To Docblocks

--- src/Models/WorkflowPriority.php
+++ src/Models/WorkflowPriority.php
@@ -6,6 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Workflowable\WorkflowEngine\Traits\HasFactory;
 
+/**
+ * Workflowable\WorkflowEngine\Models\WorkflowPriority
+ *
+ * @method static \Workflowable\WorkflowEngine\Database\Factories\WorkflowPriorityFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority query()
+ * @property int $id
+ * @property string $name
+ * @property int $priority
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Workflowable\WorkflowEngine\Models\Workflow> $workflows
+ * @property-read int|null $workflows_count
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority wherePriority($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkflowPriority whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class WorkflowPriority extends Model
 {
     use HasFactory;
