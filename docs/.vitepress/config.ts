158cd0d114a6bb9be19ed3d82660bd2e86145e96
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-07-11 17:57:27 -0400

    Fixes

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
     title: "Workflowable",
     description: "A Workflowable workflow engine",
-    base: "https://workflowable.github.io/workflowable/",
+    base: "/",
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
@@ -15,22 +15,22 @@ export default defineConfig({
             {
                 text: 'Concepts',
                 items: [
-                    { text: 'Workflow Events', link: 'concepts#workflow-events' },
-                    { text: 'Workflows', link: 'concepts#workflows' },
-                    { text: 'Workflow Priorities', link: 'concepts#workflow-priorities' },
-                    { text: 'Workflow Steps', link: 'concepts#workflow-steps' },
-                    { text: 'Workflow Transitions', link: 'concepts#workflow-transitions' },
-                    { text: 'Workflow Runs', link: 'concepts#workflow-runs' },
+                    { text: 'Workflow Events', link: '/concepts#workflow-events' },
+                    { text: 'Workflows', link: '/concepts#workflows' },
+                    { text: 'Workflow Priorities', link: '/concepts#workflow-priorities' },
+                    { text: 'Workflow Steps', link: '/concepts#workflow-steps' },
+                    { text: 'Workflow Transitions', link: '/concepts#workflow-transitions' },
+                    { text: 'Workflow Runs', link: '/concepts#workflow-runs' },
                 ]
             },
-            {
+            /*{
                 text: 'Setup',
                 items: [
-                    { text: 'Triggering Workflow Events', link: 'commands' },
-                    { text: 'Dispatching Workflow Runs', link: 'data-dependencies' },
-                    { text: 'Infrastructure', link: 'race-conditions.md' },
+                    { text: 'Triggering Workflow Events', link: '/commands' },
+                    { text: 'Dispatching Workflow Runs', link: '/data-dependencies' },
+                    { text: 'Infrastructure', link: '/race-conditions' },
                 ]
-            },
+            },*/
             {
                 text: 'Advanced',
                 items: [
