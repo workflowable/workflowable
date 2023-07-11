2cfb3314fea7b0a98930d102b18f26c66646f136
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-07-11 17:59:09 -0400

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
     title: "Workflowable",
     description: "A Workflowable workflow engine",
-    base: "/",
+    base: "/workflowable",
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
