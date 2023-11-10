df442815e493890d6651c51def432bf9483d5f3f
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-11-10 15:44:59 -0500

    Workflow Swaps [Middleware]

--- src/Jobs/WorkflowSwapRunnerJob.php
+++ src/Jobs/WorkflowSwapRunnerJob.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Workflowable\Workflowable\Middleware\CannotSwapWithRunningWorkflowProcesses;
 use Workflowable\Workflowable\Models\WorkflowSwap;
 
 class WorkflowSwapRunnerJob implements ShouldQueue
@@ -16,5 +17,17 @@ class WorkflowSwapRunnerJob implements ShouldQueue
     public function __construct(public WorkflowSwap $workflowSwap)
     {
         //
+    }
+
+    public function middleware(): array
+    {
+        return [
+            new CannotSwapWithRunningWorkflowProcesses(),
+        ];
+    }
+
+    public function handle(): void
+    {
+        // Look for workflow processes that are currently not completed
     }
 }
