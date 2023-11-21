2b9d5e07eae28687b8d81dd506c5a99afa4e1954
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2023-11-21 16:02:01 -0500

    Added Transaction To Scheduling Of Workflow Swap

--- src/Actions/WorkflowSwaps/ScheduleWorkflowSwapAction.php
+++ src/Actions/WorkflowSwaps/ScheduleWorkflowSwapAction.php
@@ -3,6 +3,7 @@
 namespace Workflowable\Workflowable\Actions\WorkflowSwaps;
 
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Workflowable\Workflowable\Abstracts\AbstractAction;
 use Workflowable\Workflowable\Enums\WorkflowSwapStatusEnum;
 use Workflowable\Workflowable\Events\WorkflowSwaps\WorkflowSwapScheduled;
@@ -11,24 +12,21 @@ use Workflowable\Workflowable\Models\WorkflowSwap;
 
 class ScheduleWorkflowSwapAction extends AbstractAction
 {
-    /**
-     * @throws WorkflowSwapException
-     */
     public function handle(WorkflowSwap $workflowSwap, Carbon $scheduledAt): WorkflowSwap
     {
-        if (! in_array($workflowSwap->workflow_swap_status_id, [
-            WorkflowSwapStatusEnum::Draft,
-            WorkflowSwapStatusEnum::Scheduled,
-        ])) {
-            throw WorkflowSwapException::workflowSwapNotEligibleForScheduling();
-        }
+        return DB::transaction(function () use ($workflowSwap, $scheduledAt) {
+            if (! in_array($workflowSwap->workflow_swap_status_id, [
+                WorkflowSwapStatusEnum::Draft,
+                WorkflowSwapStatusEnum::Scheduled,
+            ])) {
+                throw WorkflowSwapException::workflowSwapNotEligibleForScheduling();
+            }
 
-        $workflowSwap->workflowSwapStatus()->associate(WorkflowSwapStatusEnum::Scheduled->value);
-        $workflowSwap->scheduled_at = $scheduledAt;
-        $workflowSwap->save();
+            $workflowSwap->workflowSwapStatus()->associate(WorkflowSwapStatusEnum::Scheduled->value);
+            $workflowSwap->scheduled_at = $scheduledAt;
+            $workflowSwap->save();
 
-        WorkflowSwapScheduled::dispatch($workflowSwap);
-
-        return $workflowSwap;
+            WorkflowSwapScheduled::dispatch($workflowSwap);
+        });
     }
 }
